34c1881f8abf4a95f4b1cb0dcea29c74af99900d
alwin.zawadzki <alwin_zawadzki@ens.univ-artois.fr>
2025-11-12 17:06:53 +0100

    Ajout des modeles pour les communautés #3

---
+++ src/main/java/fr/univartois/butinfo/s5/api_rest/model/Community.java
@@ -0,0 +1,49 @@
+package fr.univartois.butinfo.s5.api_rest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "communities")
+public class Community {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String name;
+    private String description;
+    private String avatarUrl;
+
+    /**
+     * Liste des ID des utilisateurs qui gèrent la communauté.
+     * (Standardisé à partir de votre champ 'admin' du JSON)
+     */
+    private List<String> adminIds;
+
+    /**
+     * Nombre total de membres (dénormalisé).
+     * (Votre champ 'membersCount' du JSON)
+     */
+    private int memberCount;
+
+    private List<String> topics;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
