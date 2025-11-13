d92e4af2f7882f643fbc965bda7224fcbde26a2d
alwin.zawadzki <alwin_zawadzki@ens.univ-artois.fr>
2025-11-13 13:49:49 +0100

    Créations des dto pour les blockages #2

---
+++ src/main/java/fr/univartois/butinfo/s5/api_rest/dto/blocked/BlockedUserDto.java
@@ -0,0 +1,16 @@
+package fr.univartois.butinfo.s5.api_rest.dto.blocked;
+
+import fr.univartois.butinfo.s5.api_rest.dto.user.UserSummaryDto;
+
+import java.time.LocalDateTime;
+
+/**
+ * DTO (sortie) pour lister un utilisateur bloqué.
+ */
+public record BlockedUserDto(
+        String id,
+        UserSummaryDto user, // La personne qui est bloquée
+        String reason,
+        LocalDateTime createdAt
+) {
+}
