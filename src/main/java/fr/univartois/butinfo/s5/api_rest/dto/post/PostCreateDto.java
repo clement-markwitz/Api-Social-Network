b10e1e5f72d09f6fceee5cf8597718e6c4aa01f5
alwin.zawadzki <alwin_zawadzki@ens.univ-artois.fr>
2025-11-13 13:52:00 +0100

    Créations des dto pour les posts #2

---
+++ src/main/java/fr/univartois/butinfo/s5/api_rest/dto/post/PostCreateDto.java
@@ -0,0 +1,27 @@
+package fr.univartois.butinfo.s5.api_rest.dto.post;
+
+import fr.univartois.butinfo.s5.api_rest.model.PostType;
+import fr.univartois.butinfo.s5.api_rest.model.PostVisibility;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * DTO (entrée) pour la création d'un nouveau Post.
+ */
+public record PostCreateDto(
+        @NotBlank
+        String text,
+
+        MediaDto media, // Le client envoie les URLs des médias uploadés
+
+        @NotNull
+        PostType type, // Le client doit spécifier le type
+
+        @NotNull
+        PostVisibility visibility, // Le client doit spécifier la visibilité
+
+        // Le post peut être lié à une Page OU une Communauté, ou aucun (post personnel)
+        String pageId,
+        String communityId
+) {
+}
