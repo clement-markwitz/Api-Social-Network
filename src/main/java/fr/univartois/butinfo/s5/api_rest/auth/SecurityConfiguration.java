df041ed7e6804d192e6f3e85855ceced25844923
BenjaminC62 <benjamincornet62130@gmail.com>
2025-12-16 16:10:43 +0100

    #36 - Ajout de nouvelles autorisations pour les requêtes GET sur les endpoints /api/pages, /api/posts et /api/users dans la configuration de sécurité

--- src/main/java/fr/univartois/butinfo/s5/api_rest/auth/SecurityConfiguration.java
+++ src/main/java/fr/univartois/butinfo/s5/api_rest/auth/SecurityConfiguration.java
@@ -33,14 +33,15 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/community/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/pages/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/users/**").permitAll()
                         .requestMatchers(
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
                         ).permitAll()
                         .requestMatchers("/api/users/ban/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN")
 
                         .anyRequest().authenticated()
                 )
