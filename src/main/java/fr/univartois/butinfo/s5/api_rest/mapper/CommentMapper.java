4df0a0e995867594b7a7ec60c30ceee5a34c7a04
mathias_morel <mathias_morel@ens.univ-artois.fr>
2025-12-16 10:26:59 +0100

    #38 Ajout du CommentMapper

---
+++ src/main/java/fr/univartois/butinfo/s5/api_rest/mapper/CommentMapper.java
@@ -0,0 +1,34 @@
+package fr.univartois.butinfo.s5.api_rest.mapper;
+
+import fr.univartois.butinfo.s5.api_rest.dto.comment.CommentCreateDto;
+import fr.univartois.butinfo.s5.api_rest.dto.comment.CommentDto;
+import fr.univartois.butinfo.s5.api_rest.model.Comment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "post", ignore = true)
+    @Mapping(target = "author", ignore = true)
+    @Mapping(target = "likeCount", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "parentComment", source = "parentCommentId", qualifiedByName = "idToComment")
+    Comment toEntity(CommentCreateDto dto);
+
+    @Mapping(target = "postId", source = "post.id")
+    @Mapping(target = "authorId", source = "author.id")
+    @Mapping(target = "parentCommentId", source = "parentComment.id")
+    CommentDto toDto(Comment comment);
+
+    @Named("idToComment")
+    default Comment idToComment(String id) {
+        if (id == null) return null;
+        Comment comment = new Comment();
+        comment.setId(id);
+        return comment;
+    }
+}
