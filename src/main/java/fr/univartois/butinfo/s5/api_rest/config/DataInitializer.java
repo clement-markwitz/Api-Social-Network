b50796dd3b6cbae22eacef2333afc1adedb14098
clement_markwitz <clement_markwitz@ens.univ-artois.fr>
2025-12-19 11:20:59 +0100

    ajout de dataInitializer pour creer un admin robert duchmol

---
+++ src/main/java/fr/univartois/butinfo/s5/api_rest/config/DataInitializer.java
@@ -0,0 +1,43 @@
+package fr.univartois.butinfo.s5.api_rest.config;
+
+import fr.univartois.butinfo.s5.api_rest.dto.user.UserCreateDto;
+import fr.univartois.butinfo.s5.api_rest.model.User;
+import fr.univartois.butinfo.s5.api_rest.repository.UserRepository;
+import fr.univartois.butinfo.s5.api_rest.auth.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class DataInitializer {
+
+    private final AuthenticationService authenticationService;
+    private final UserRepository userRepository;
+
+    @Bean
+    public CommandLineRunner initAdmin() {
+        return args -> {
+            String adminUsername = "robert duchmol";
+
+            if (userRepository.findByUsername(adminUsername).isPresent()) {
+                return;
+            }
+
+            UserCreateDto adminDto = new UserCreateDto(
+                    adminUsername,
+                    "robert_duchmol@gmail.com",
+                    "GrosSecret1234!",
+                    "RobertD"
+            );
+
+            User admin = authenticationService.register(adminDto);
+
+            admin.setRole("ADMIN");
+
+            userRepository.save(admin);
+        };
+    }
+}
