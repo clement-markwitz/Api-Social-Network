03c8e896965803c26aa2406a68cc83bfd2422a50
alwin.zawadzki <alwin_zawadzki@ens.univ-artois.fr>
2026-01-09 14:43:35 +0100

    Corrections des recommendations de users, pages, posts

--- src/main/java/fr/univartois/butinfo/s5/api_rest/repository/PageRepository.java
+++ src/main/java/fr/univartois/butinfo/s5/api_rest/repository/PageRepository.java
@@ -1,8 +1,11 @@
 package fr.univartois.butinfo.s5.api_rest.repository;
 
 import fr.univartois.butinfo.s5.api_rest.model.Page;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Repository interface for managing Page entities in MongoDB.
@@ -16,4 +19,14 @@ public interface PageRepository extends MongoRepository<Page, String> {
      * @return true if a page with the given name exists, false otherwise
      */
     boolean existsByName(String name);
+
+    /**
+     * Retrieve a list of random pages.
+     *
+     * @return a list of random Page entities
+     */
+    @Aggregation(pipeline = {
+            "{ '$sample': { 'size': ?0 } }"
+    })
+    List<Page> findRandomPages(int count);
 }
