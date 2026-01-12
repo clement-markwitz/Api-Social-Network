447b7b23cf2cc7bb313e7bad15b083b9816d45a3
Cornet Benjamin <benjamincornet62130@gmail.com>
2026-01-12 10:57:00 +0100

    chagement du update page dto

--- src/main/java/fr/univartois/butinfo/s5/api_rest/dto/page/PageUpdateDto.java
+++ src/main/java/fr/univartois/butinfo/s5/api_rest/dto/page/PageUpdateDto.java
@@ -7,9 +7,14 @@ import java.util.List;
  * DTO for updating page information.
  */
 public record PageUpdateDto(
+        @Size(min = 3, max = 100)
+        String name,
+
         @Size(max = 500)
         String description,
 
-        List<String> topics
+        List<String> topics,
+
+        String avatarUrl
 ) {
 }
